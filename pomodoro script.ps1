cf295a2ab74eff0d21bc5ebbb7e54578398cf837
tarkangel <angel_calderon5@hotmail.com>
2022-07-11 20:04:39 -0500

    Updated

--- pomodoro script.ps1
+++ pomodoro script.ps1
@@ -1,43 +1,44 @@
 ###################################################################################
-# .\"pomodoro script.ps1"  -WindowStyle hidden
+# to execute your script run this command in powershell -->>  .\"pomodoro script.ps1"  -WindowStyle hidden
 
-$operaPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher.exe"
-$workMusic = "https://youtu.be/vfdwtIOD1Vw?t=1"
-$breakMusic = "https://youtu.be/00w10A2Vt1Y?t=1"
+$browserPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher.exe"
+$workMusicLink = "https://youtu.be/vfdwtIOD1Vw?t=1"
+$breakMusicLink = "https://youtu.be/00w10A2Vt1Y?t=1"
+$browserName = "opera"
 
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
 Start-Sleep -Seconds 1200
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
 Start-Sleep -Seconds 300
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
 Start-Sleep -Seconds 1200
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
 Start-Sleep -Seconds 300
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
 Start-Sleep -Seconds 1200
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
 Start-Sleep -Seconds 300
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
 Start-Sleep -Seconds 1200
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
 Start-Sleep -Seconds 900
-Stop-Process -Name "opera"
+Stop-Process -Name $browserName
 
