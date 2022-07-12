9754e01165749753ac5cadb7cbc4413c3dc69301
tarkangel <angel_calderon5@hotmail.com>
2022-07-11 21:08:31 -0500

    updates

--- pomodoro script.ps1
+++ pomodoro script.ps1
@@ -1,50 +1,43 @@
 ###################################################################################
 #Pomodoro Studying Technique
 # to execute your script run this command in powershell -->> "
-# $processName ="Pomodoro" ;$scriptPath = "C:\Users\Angel PC Gamer\Documents\pomodoro script.ps1"; Start-Job -Name $processName -FilePath $scriptPath
+# $processName ="Pomodoro" ;$scriptPath = "wsl$\Ubuntu-20.04\home\tarkangel\gitRepos\powershell\pomodoro script.ps1"; Start-Job -Name $processName -FilePath $scriptPath
 #to stop the script run this :
 # $processName="Pomodoro" ; Stop-Process -Name $processName
 
 
-$scriptPath = "C:\Users\Angel PC Gamer\Documents\"pomodoro script.ps1"
+#$scriptPath = "wsl$\Ubuntu-20.04\home\tarkangel\gitRepos\powershell\pomodoro script.ps1"
 $browserPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher.exe"
-$workMusicLink = "https://youtu.be/vfdwtIOD1Vw?t=1"
-$breakMusicLink = "https://youtu.be/00w10A2Vt1Y?t=1"
+$workMusicLinkLink = "https://youtu.be/vfdwtIOD1Vw?t=1"
+$breakMusicLinkLink = "https://youtu.be/00w10A2Vt1Y?t=1"
 $browserName = "opera"
 
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $workMusicLink ; Start-Sleep -Seconds 1500
+
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
+Start-Sleep -Seconds 300
 Stop-Process -Name $browserName
-Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1500
-Stop-Process -Name $browserName
-Start-Sleep -Seconds 1
-Start-Process -FilePath $operaPath -ArgumentList $breakMusic
+
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $workMusicLink ; Start-Sleep -Seconds 1500
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
 Start-Sleep -Seconds 300
 Stop-Process -Name $browserName
 
 Stop-Process -Name $browserName
-Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1500
-Stop-Process -Name $browserName
-Start-Sleep -Seconds 1
-Start-Process -FilePath $operaPath -ArgumentList $breakMusic
+Start-Process -FilePath $browserPath -ArgumentList $workMusicLink ; Start-Sleep -Seconds 1500
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
 Start-Sleep -Seconds 300
 Stop-Process -Name $browserName
 
 Stop-Process -Name $browserName
-Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1500
-Stop-Process -Name $browserName
-Start-Sleep -Seconds 1
-Start-Process -FilePath $operaPath -ArgumentList $breakMusic
-Start-Sleep -Seconds 300
-Stop-Process -Name $browserName
-
-Stop-Process -Name $browserName
-Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1500
-Stop-Process -Name $browserName
-Start-Sleep -Seconds 1
-Start-Process -FilePath $operaPath -ArgumentList $breakMusic
+Start-Process -FilePath $browserPath -ArgumentList $workMusicLink ; Start-Sleep -Seconds 1500
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
 Start-Sleep -Seconds 900
 Stop-Process -Name $browserName
 
