7789a9eff5af696fe331d998b88ba3676267155b
tarkangel <angel_calderon5@hotmail.com>
2022-07-11 20:53:03 -0500

    updates

--- pomodoro script.ps1
+++ pomodoro script.ps1
@@ -1,6 +1,12 @@
 ###################################################################################
-# to execute your script run this command in powershell -->>  .\"pomodoro script.ps1"  -WindowStyle hidden
+#Pomodoro Studying Technique
+# to execute your script run this command in powershell -->> "
+# $processName ="Pomodoro" ;$scriptPath = "C:\Users\Angel PC Gamer\Documents\pomodoro script.ps1"; Start-Job -Name $processName -FilePath $scriptPath
+#to stop the script run this :
+# $processName="Pomodoro" ; Stop-Process -Name $processName
 
+
+$scriptPath = "C:\Users\Angel PC Gamer\Documents\"pomodoro script.ps1"
 $browserPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher.exe"
 $workMusicLink = "https://youtu.be/vfdwtIOD1Vw?t=1"
 $breakMusicLink = "https://youtu.be/00w10A2Vt1Y?t=1"
@@ -8,7 +14,7 @@ $browserName = "opera"
 
 Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1200
+Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
@@ -17,7 +23,7 @@ Stop-Process -Name $browserName
 
 Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1200
+Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
@@ -26,7 +32,7 @@ Stop-Process -Name $browserName
 
 Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1200
+Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
@@ -35,7 +41,7 @@ Stop-Process -Name $browserName
 
 Stop-Process -Name $browserName
 Start-Process -FilePath $operaPath -ArgumentList $workMusic
-Start-Sleep -Seconds 1200
+Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName
 Start-Sleep -Seconds 1
 Start-Process -FilePath $operaPath -ArgumentList $breakMusic
