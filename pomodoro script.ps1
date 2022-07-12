d8e1c77728721ce4355dca70183cf0d3b06f67aa
tarkangel <83728906+tarkangel@users.noreply.github.com>
2022-07-11 19:10:11 -0500

    Create pomodoro script.ps1

---
+++ pomodoro script.ps1
@@ -0,0 +1,15 @@
+###################################################################################
+# .\"pomodoro script.ps1"  -WindowStyle hidden
+
+$operaPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher.exe"
+$workMusic = "https://youtu.be/vfdwtIOD1Vw?t=1"
+$breakMusic = "https://youtu.be/00w10A2Vt1Y?t=1"
+
+Stop-Process -Name "opera"
+Start-Process -FilePath $operaPath -ArgumentList $workMusic
+Start-Sleep -Seconds 1200
+Stop-Process -Name "opera"
+Start-Sleep -Seconds 1
+Start-Process -FilePath $operaPath -ArgumentList $breakMusic
+Start-Sleep -Seconds 300
+Stop-Process -Name "opera"
