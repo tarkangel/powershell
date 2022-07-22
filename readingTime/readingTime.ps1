a370a9c47b2a61ed404cac819d32d1d8b4d35ec1
tarkangel <83728906+tarkangel@users.noreply.github.com>
2022-07-22 17:40:12 -0500

    Update readingTime.ps1

--- readingTime/readingTime.ps1
+++ readingTime/readingTime.ps1
@@ -8,7 +8,50 @@ $browserPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher
 $readingMusicLink = "https://youtu.be/mIYzp5rcTvU"
 $browserName = "opera"
 
+function timerWindow ([int]$delay)
+ {
+
+$Counter_Form = New-Object System.Windows.Forms.Form
+$Counter_Form.Text = "Pomodoro Timer"
+$Counter_Form.Width = 450
+$Counter_Form.Height = 200
+$Counter_Form.AutoSize = $true
+$Counter_Form.Font = New-Object System.Drawing.Font("Times New Roman",12)
+$Counter_Form.BackColor = "#6CA2FF"
+$Counter_Form.WindowState = "Normal"
+$Counter_Label = New-Object System.Windows.Forms.Label
+$Counter_Label.AutoSize = $true 
+$Counter_Form.Controls.Add($Counter_Label)
+while ($delay -gt 0)
+{
+  $Counter_Form.Show()
+  $minutes = [Math]::Floor(([int]$delay / 60))
+  if ($minutes -ge 1 )
+    { 
+        $seconds = $delay - ($minutes*60)   
+    }
+    else
+    {
+        $seconds = [int]$delay
+    }
+  if ($minutes -le 9)
+  {
+    $minutes = "0$($minutes)"
+  }
+  if ($seconds -le 9)
+  {
+    $seconds = "0$($seconds)"
+  }
+  
+  $time = "$($minutes):$($seconds)"
+  $Counter_Label.Text = "Time Remaining :    $($time) "
+  start-sleep 1
+  $delay = ($delay - 1)
+}
+$Counter_Form.Close() 
+}
+
 Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
 Start-Process -FilePath $browserPath -ArgumentList $readingMusicLink
-Start-Sleep -Seconds 1800
+timerWindow(1800)
 Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
