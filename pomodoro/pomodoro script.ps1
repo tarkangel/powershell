8ac2115adcd0b9f3f4a23e58656d6d4ca406aeb0
tarkangel <angel_calderon5@hotmail.com>
2022-07-22 10:41:01 -0500

    Quick Update:

--- pomodoro/pomodoro script.ps1
+++ pomodoro/pomodoro script.ps1
@@ -2,35 +2,29 @@
 #Pomodoro Studying Technique
 #Please read README.md to set up the script
 # to execute your script run this command in powershell -->> "
-# $processName ="Pomodoro" ;$scriptPath = "\\wsl$\Ubuntu-20.04\home\tarkangel\gitRepos\powershell\pomodoro script.ps1"; Start-Job -Name $processName -FilePath $scriptPath
+# $scriptPath = "\\wsl$\Ubuntu-20.04\home\tarkangel\gitRepos\powershell\pomodoro\pomodoro_script.ps1"; Start-Job -FilePath $scriptPath
 #to stop the script run this :
 # $processName="Pomodoro" ; Stop-Process -Name $processName
 
 
-#$scriptPath = "wsl$\Ubuntu-20.04\home\tarkangel\gitRepos\powershell\pomodoro script.ps1"
+#$scriptPath = "wsl$\Ubuntu-20.04\home\tarkangel\gitRepos\powershell\pomodoro_script.ps1"
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
 $browserPath = "C:\Users\Angel PC Gamer\AppData\Local\Programs\Opera GX\launcher.exe"
-$workMusicLink = "https://youtu.be/M5QY2_8704o?t=1"
-$breakMusicLink = "https://youtu.be/-8N9UR6OTCs?t=1"
+$workMusicLink = "https://youtu.be/M5QY2_8704o?t="
+$breakMusicLink = "https://youtu.be/-8N9UR6OTCs?t="
 $browserName = "opera"
-
 Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
-Start-Process -FilePath $browserPath -ArgumentList $workMusicLink
+Start-Process -FilePath $browserPath -ArgumentList ($workMusicLink + "1")
 Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
 Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
 Start-Sleep -Seconds 300
 Stop-Process -Name $browserName
 
-Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
-Start-Process -FilePath $browserPath -ArgumentList $workMusicLink
-Start-Sleep -Seconds 1500
-Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
-Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
-Start-Sleep -Seconds 300
-Stop-Process -Name $browserName
 
-Stop-Process -Name $browserName
-Start-Process -FilePath $browserPath -ArgumentList $workMusicLink
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList ($workMusicLink + "1560")
 Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
 Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
@@ -38,7 +32,15 @@ Start-Sleep -Seconds 300
 Stop-Process -Name $browserName
 
 Stop-Process -Name $browserName
-Start-Process -FilePath $browserPath -ArgumentList $workMusicLink
+Start-Process -FilePath $browserPath -ArgumentList ($workMusicLink + "3000")
+Start-Sleep -Seconds 1500
+Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
+Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
+Start-Sleep -Seconds 300
+Stop-Process -Name $browserName
+
+Stop-Process -Name $browserName
+Start-Process -FilePath $browserPath -ArgumentList ($workMusicLink + "4500")
 Start-Sleep -Seconds 1500
 Stop-Process -Name $browserName ; Start-Sleep -Seconds 2
 Start-Process -FilePath $browserPath -ArgumentList $breakMusicLink
