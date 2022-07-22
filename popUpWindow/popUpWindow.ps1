6a4b6fd7c4f4f7be3d953016c5d4d3584f2ceb19
tarkangel <angel_calderon5@hotmail.com>
2022-07-22 10:36:16 -0500

    Updated

--- popUpWindow/popUpWindow.ps1
+++ popUpWindow/popUpWindow.ps1
@@ -1,18 +1,12 @@
-﻿$delay = 5
-$counter = 0
+﻿$delay = 10
 $Counter_Form = New-Object System.Windows.Forms.Form
-$Counter_Form.Text = "Pomodoro Timer $counter"
-$Counter_Form.BackColor = "#6CA2FF"
-$Counter_Form.
-#$Counter_Form.Width = 450
-#$Counter_Form.Height = 200
-$Counter_Form.ClientSize = '450,200'
-$Counter_Form.MaximizeBox = true 
+$Counter_Form.Text = "Countdown Timer!"
+$Counter_Form.Width = 450
+$Counter_Form.Height = 200
 $Counter_Label = New-Object System.Windows.Forms.Label
-$Counter_Label.AutoSize = $true
-
+$Counter_Label.AutoSize = $true 
 $Counter_Form.Controls.Add($Counter_Label)
-while ($delay -gt 0)
+while ($delay -ge 0)
 {
   $Counter_Form.Show()
   $Counter_Label.Text = "Seconds Remaining: $($delay)"
